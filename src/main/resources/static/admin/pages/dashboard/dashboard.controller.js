6c5d70b924aa839d74510e288e34f1dde4dee419
AnonymousDude <balisongian@gmail.com>
2022-11-04 09:59:44 +0700

    admin product - add search input ở list.html

--- src/main/resources/static/admin/pages/dashboard/dashboard.controller.js
+++ src/main/resources/static/admin/pages/dashboard/dashboard.controller.js
@@ -1,15 +1,3 @@
 app.controller("dashboard-ctrl", function ($scope, $http) {
-    $scope.user = {};
 
-    $scope.initialize = function () {
-        $http.get("/rest/users/principal").then(resp => {
-            $scope.user = resp.data;
-            localStorage.setItem("user", JSON.stringify(resp.data.id));
-        }).catch(error => {
-            console.log("Error", error);
-        });
-
-    };
-
-    $scope.initialize();
 });
