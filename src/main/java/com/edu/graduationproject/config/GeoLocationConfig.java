8c3f3c6b022c30fbe806a166a0200dc0a8233f6a
AnonymousDude <balisongian@gmail.com>
2022-11-11 11:44:15 +0700

    thêm fields Visitor.java, fix VisitorLogger.java, thêm GeoLite2-City.mmdb

---
+++ src/main/java/com/edu/graduationproject/config/GeoLocationConfig.java
@@ -0,0 +1,42 @@
+package com.edu.graduationproject.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+import com.maxmind.db.Reader;
+import com.maxmind.geoip2.DatabaseReader;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+public class GeoLocationConfig {
+    private static DatabaseReader reader = null;
+    private final ResourceLoader resourceLoader;
+
+    public GeoLocationConfig(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
+    @Bean
+    public DatabaseReader databaseReader() {
+        try {
+            Resource resource = resourceLoader.getResource("classpath:maxmind/GeoLite2-City.mmdb");
+            InputStream dbAsStream = resource.getInputStream();
+            System.out.println("-----GeoLocationConfig loaded successfully!----");
+
+            return reader = new DatabaseReader.Builder(dbAsStream)
+                    .fileMode(Reader.FileMode.MEMORY)
+                    .build();
+        } catch (IOException | NullPointerException e) {
+            System.out.println("-----Datbase loader could not be initiated!!----");
+            return null;
+        }
+    }
+
+}
